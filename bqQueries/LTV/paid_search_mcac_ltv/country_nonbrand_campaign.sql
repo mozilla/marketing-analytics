7eec971960d8e6276088ae78fbd27e565ba59505
zerokarmaleft <zerokarmaleft@gmail.com>
2018-10-18 17:20:11 -0500

    refactor country_nonbrand_campaign

--- bqQueries/LTV/paid_search_mcac_ltv/country_nonbrand_campaign.sql
+++ bqQueries/LTV/paid_search_mcac_ltv/country_nonbrand_campaign.sql
@@ -1,160 +1,119 @@
- --Pulls together data from Fetch, GA, Corpmetrics and Telemetry to tell the story of Spend, downloads, installs, LTV
-WITH
-  --Data pull from telemetry.corpmetrics as the root for our data system. Data from other systems will be joined to this.
-  telCorp AS (
-  SELECT
-    submission_date_s3,
-    REPLACE(REPLACE(REPLACE(REPLACE(contentCleaned,'%2528','('),'%2529',')'),'%2B',' '),'%257C','|') t_content,
-    SUM(installs) AS sum_installs,
-    SUM(dau) AS sum_dau
-  FROM
-    `telemetry.corpMetrics`
-  GROUP BY
-    1,
-    2 ),
-  --Joins with LTV data set based on source, medium, campaign, and content
-  l AS (
+-- Pulls together data from Fetch, GA, Corpmetrics and Telemetry to tell the
+-- story of Spend, downloads, installs, LTV
+with
+  -- Limit Fetch data to 1) Non-branded Search in 2) top countries, 3) specific
+  -- vendors, and within 4) the current quarter
+  nonbranded_search_spend AS (
+    SELECT
+      date,
+      vendor,
+      adname,
+      REGEXP_EXTRACT(socialstring, r'(.*)_') AS socialstring,
+      country,
+      targeting,
+      vendornetspend,
+      downloadsGA
+    FROM
+      `fetch.fetch_deduped`
+    WHERE
+      vendor IN ('Adwords', 'Bing')
+      AND country IN ('United States', 'Canada', 'Germany')
+      AND targeting = 'Nonbrand Search'
+      AND vendornetspend > 0
+      AND date BETWEEN DATE(2018, 10, 1) AND DATE(2018, 12, 31)
+  ),
+
+  -- Join Fetch with LTV based on source, medium, campaign, and content
+  ltv_attribution AS (
+    SELECT
+      f.country,
+      f.targeting,
+      f.socialstring,
+      f.vendor,
+      COUNT(DISTINCT(ltv.client_ID)) AS n,
+      AVG(ltv.total_clv) AS avg_tLTV
+    FROM
+      `ltv.v1_clients_20181017` AS ltv
+    LEFT JOIN
+      nonbranded_search_spend AS f
+    ON
+      ltv.content = f.adname
+    WHERE
+      -- historical_searches < 5 stddevs away from mean
+      ltv.historical_searches < (
+        SELECT
+          STDDEV(historical_searches) * 5
+        FROM
+          `ltv.v1_clients_20181017`
+      ) + (
+        SELECT
+          AVG(historical_searches)
+        FROM
+          `ltv.v1_clients_20181017`
+      )
+    GROUP BY
+      f.country,
+      f.targeting,
+      f.socialstring,
+      f.vendor
+  )
+
+-- Pulls whole table
+SELECT
+  spending.country,
+  spending.targeting,
+  spending.socialstring,
+  spending.vendor,
+  spending.sum_vendornetspend,
+  spending.sum_fetchdownloads,
+  spending.proj_installs,
+  spending.CPD,
+  spending.proj_cpi,
+  ltv_attribution.n,
+  ltv_attribution.avg_tLTV,
+  ltv_attribution.avg_tLTV * spending.proj_installs AS revenue,
+  (ltv_attribution.avg_tLTV * spending.proj_installs) - spending.sum_vendornetspend AS profit,
+  (ltv_attribution.avg_tLTV * spending.proj_installs) / spending.sum_vendornetspend AS ltv_mcac,
+  CASE
+    WHEN (ltv_attribution.avg_tLTV * spending.proj_installs) = 0 THEN 0
+    ELSE spending.sum_vendornetspend / (ltv_attribution.avg_tLTV * spending.proj_installs)
+  END AS mcac_ltv
+FROM (
+  -- group aggregations by week, country, targeting, and socialstring
   SELECT
     f.country,
     f.targeting,
+    f.socialstring,
+    f.vendor,
+    SUM(f.vendorNetSpend) AS sum_vendornetspend,
+    SUM(f.downloadsGA) AS sum_fetchdownloads,
+    SUM(f.downloadsGA) * .66 AS proj_installs,
     CASE
-      WHEN REGEXP_EXTRACT(socialstring, r'(.*)_') LIKE '%Competitor%' THEN 'Competitor'
-      WHEN REGEXP_EXTRACT(socialstring, r'(.*)_') LIKE '%Browser%' THEN 'Browser'
-    END AS AGroup,
-    COUNT(DISTINCT(client_ID)) n,
-    AVG(total_clv) avg_tLTV
+      WHEN SUM(f.downloadsGA) = 0 THEN 0
+      ELSE SUM(f.vendornetspend) / SUM(f.downloadsGA)
+    END AS CPD,
+    CASE
+      WHEN SUM(f.downloadsGA) = 0 THEN 0
+      ELSE SUM(f.vendornetspend) / (SUM(f.downloadsGA) * .66)
+    END AS proj_CPI
   FROM
-    `ltv.v1_clients_20180919`
-  LEFT JOIN
-    `fetch.fetch_deduped` AS f
-  ON
-    content = f.adname
-  WHERE
-    historical_searches < (
-    SELECT
-      STDDEV(historical_searches)
-    FROM
-      `ltv.v1_clients_20180919`) *5 + (
-    SELECT
-      AVG(historical_searches)
-    FROM
-      `ltv.v1_clients_20180919`)
-    AND vendor IN ('Adwords',
-      'Bing')
-    AND f.country IN ('United States',
-      'Canada',
-      'Germany')
-    AND targeting = 'Nonbrand Search'
-    AND vendornetspend >0
+    nonbranded_search_spend AS f
   GROUP BY
-    1,
-    2,
-    3
-  ORDER BY
-    1,
-    2,
-    3 DESC),
-  --Pulls VendorNetSpend by ad and day from FetchMme
-  f AS (
-  SELECT
-    --turns date into a string and removes '-'
-    *
-  FROM
-    `fetch.fetch_deduped`)
-  --Pulls whole table
-SELECT
-  country,
-  targeting,
-socialstring,
-vendor,
-sum_vendornetspend,
-  sum_fetchdownloads,
-  proj_installs,
-  CPD,
-  proj_cpi,
-  n,
-  avg_tLTV,
-  avg_tLTV * proj_installs AS revenue,
-  (avg_tLTV * proj_installs) - sum_vendornetspend AS profit,
-  (avg_tLTV * proj_installs)/sum_vendornetspend AS mcac_ltv
-FROM (
-  SELECT
- --   DATE_DIFF(Date,DATE(2018,01,03),week) as week_num,
-    Country,
-    targeting,
-socialstring,
-    vendor,
-    SUM(vendorNetSpend) sum_vendornetspend,
-    --  sum(downloads) sum_downloads,
-    SUM(downloadsGA) sum_fetchdownloads,
-    SUM(downloadsGA)*.66 proj_installs,
-    Case when sum(downloadsGA) = 0 then null else (SUM(vendornetspend)/SUM(downloadsGA)) end AS CPD,
-   Case when sum(downloadsGA) = 0 then null else ( SUM(vendornetspend)/(SUM(downloadsGA)*.66)) end AS proj_CPI
-  FROM
-    --Fetch is the base table
-    f
-  LEFT JOIN
-    telcorp
-  ON
-    REPLACE(CAST(date AS STRING),'-','') = telcorp.submission_date_s3
-    AND f.adname = telcorp.t_content
-  WHERE
-    vendor IN ('Adwords',
-      'Bing')
-    AND country IN ('United States',
-      'Canada',
-      'Germany')
-    AND REPLACE(CAST(f.date AS STRING),'-','') BETWEEN '20180901'
-    AND '20180922'
-    AND targeting = 'Nonbrand Search'
-    AND vendornetspend >0
-  GROUP BY
-    1,
-    2,3,4
-  ORDER BY
-    1,
-    2,
-    3 DESC) AS Qa
-LEFT JOIN (
-  SELECT
-    f.country AS b_country,
-    f.targeting AS b_targeting,
-    f.socialstring as b_socialstring,
-    f.vendor as b_vendor,
-    COUNT(DISTINCT(client_ID)) n,
-    AVG(total_clv) avg_tLTV
-  FROM
-    `ltv.v1_clients_20180919`
-  LEFT JOIN
-    `fetch.fetch_deduped` AS f
-  ON
-    content = f.adname
-  WHERE
-    historical_searches < (
-    SELECT
-      STDDEV(historical_searches)
-    FROM
-      `ltv.v1_clients_20180919`) *5 + (
-    SELECT
-      AVG(historical_searches)
-    FROM
-      `ltv.v1_clients_20180919`)
-    AND vendor IN ('Adwords',
-      'Bing')
-    AND f.country IN ('United States',
-      'Canada',
-      'Germany')
-    AND targeting = 'Nonbrand Search'
-    AND vendornetspend >0
-  GROUP BY
-    1,
-    2,3,4 ) AS Qb
+    f.country,
+    f.targeting,
+    f.socialstring,
+    f.vendor
+) AS spending
+LEFT JOIN
+  ltv_attribution
 ON
-  Qa.country = Qb.b_country
-  AND QA.targeting =Qb.b_targeting
-  And Qa.socialstring = Qb.b_socialstring
-  AND QA.vendor = Qb.b_vendor
+  spending.country = ltv_attribution.country
+  AND spending.targeting = ltv_attribution.targeting
+  AND spending.socialstring = ltv_attribution.socialstring
+  AND spending.vendor = ltv_attribution.vendor
 ORDER BY
-  1,
-  2,
-  3 ASC
+  spending.country,
+  spending.targeting,
+  spending.socialstring,
+  spending.vendor,
+  spending.sum_vendornetspend ASC

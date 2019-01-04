c350c8cb18f78839c4d93b2d502441870ac3660f
gkabbz <george@akisha.ca>
2019-01-04 12:34:58 -0500

    Misc changes to dailyRetention and distribution ID pulls

--- telemetry/mainSummary/distributionID/distributionIDCleanup.py
+++ telemetry/mainSummary/distributionID/distributionIDCleanup.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Open Files and store in dataframes
-file = '/Users/gkaberere/spark-warehouse/adHoc/adHoc/distributionIDPerf20181210-20181216.csv/distributionIDPerf20181210-20181216.csv'
+file = '/Users/gkaberere/spark-warehouse/adHoc/adHoc/distributionIDPerf20181224-20181230.csv/distributionIDPerf20181224-20181230.csv'
 
 df = pd.read_csv(file, sep=',', header=0, index_col=None, usecols=None, skiprows=None, encoding='utf-8')
 
 dfSoftonic = df.loc[df['distribution_id'].isin(['softonic-002', 'softonic-003'])]
 
-softonicFile = '/Users/gkaberere/spark-warehouse/adHoc/adHoc/softonic20181216.csv'
+softonicFile = '/Users/gkaberere/spark-warehouse/adHoc/adHoc/softonic20181230.csv'
 with open(softonicFile, 'w') as file:
     columnNames = ('submission_date_s3', 'distribution_id', 'country', 'DAU', 'activeDAU', 'totalURI', 'searches','installs')
     dfSoftonic.to_csv(file, index=False, header=True, encoding='utf-8')

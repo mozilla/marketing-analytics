37fea6579cb41daf8a623a13dc6a3e3931205cb5
gkabbz <george@akisha.ca>
2019-02-28 06:03:13 -0500

    Add sitemetricsData query

--- bqQueries/desktopGrowth/reporting/siteMetricsData.sql
+++ bqQueries/desktopGrowth/reporting/siteMetricsData.sql
@@ -6,12 +6,12 @@ SELECT
 
 sessionsTable as (
   SELECT
-    date,
-    country,
-    source,
-    medium,
-    campaign,
-    content,
+    PARSE_DATE('%Y%m%d', date) as date,
+    CASE WHEN country IS NULL THEN '' ELSE country END as country,
+    CASE WHEN source IS NULL THEN '' ELSE source END as source,
+    CASE WHEN medium IS NULL THEN '' ELSE medium END as medium,
+    CASE WHEN campaign IS NULL THEN '' ELSE campaign END as campaign,
+    CASE WHEN content IS NULL THEN '' ELSE content END as content,
     SUM(sessions) as sessions,
     SUM(CASE WHEN browser != 'Firefox' THEN sessions ELSE 0 END) as nonFXSessions
     FROM(
@@ -51,11 +51,11 @@ sessionsTable as (
 downloadsTable as(
 SELECT
   PARSE_DATE('%Y%m%d', date) as date,
-  country,
-  source,
-  medium,
-  campaign,
-  content,
+  CASE WHEN country IS NULL THEN '' ELSE country END as country,
+  CASE WHEN source IS NULL THEN '' ELSE source END as source,
+  CASE WHEN medium IS NULL THEN '' ELSE medium END as medium,
+  CASE WHEN campaign IS NULL THEN '' ELSE campaign END as campaign,
+  CASE WHEN content IS NULL THEN '' ELSE content END as content,
   SUM(IF(downloads > 0,1,0)) as downloads,
   SUM(IF(downloads > 0 AND browser != 'Firefox',1,0)) as nonFXDownloads
 FROM (SELECT
@@ -81,6 +81,45 @@ WHERE
 GROUP BY
   date,visitorID, visitNumber, country, source, medium, campaign, content, browser)
 GROUP BY date, country, source, medium, campaign, content
+),
+
+siteData as (
+SELECT
+  sessionsTable.date,
+  sessionsTable.country,
+  sessionsTable.source,
+  sessionsTable.medium,
+  sessionsTable.campaign,
+  sessionsTable.content,
+  SUM(sessionsTable.sessions) as sessions,
+  SUM(sessionsTable.nonFXSessions) as nonFXSessions,
+  SUM(downloadsTable.downloads) as downloads,
+  SUM(downloadsTable.nonFXDownloads) as nonFXDownloads
+FROM
+  sessionsTable
+LEFT JOIN
+  downloadsTable
+ON
+  sessionsTable.date = downloadsTable.date
+  AND sessionsTable.country = downloadsTable.country
+  AND sessionsTable.source = downloadsTable.source
+  AND sessionsTable.medium = downloadsTable.medium
+  AND sessionsTable.campaign = downloadsTable.campaign
+  AND sessionsTable.content = downloadsTable.content
+GROUP BY
+  date, country, source, medium, campaign, content
+),
+
+cleanedSiteData as (
+SELECT
+  siteData.*,
+  standardizedCountryList.standardizedCountry as countryCleaned
+FROM
+  siteData
+LEFT JOIN
+  `lookupTables.standardizedCountryList` as standardizedCountryList
+ON
+  siteData.country = standardizedCountryList.rawCountry
 )
 
-SELECT source, sum(sessions) as sessions, sum(nonFXSessions) as nonFXSessions FROM sessionsTable GROUP BY 1 ORDER BY 2 DESC
+SELECT * from cleanedSiteData

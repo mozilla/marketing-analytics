1ca11184976dd1411a7d747e0c47160b79dd1935
gkabbz <george@akisha.ca>
2019-05-29 10:46:13 -0400

    Move sitemetrics summary tables jobs to app engine folder

--- AppEngine-moz-mktg-prod-001/siteMetricsByLandingPageSummaryTable.py
+++ AppEngine-moz-mktg-prod-001/siteMetricsByLandingPageSummaryTable.py
@@ -1,6 +1,5 @@
 # This job pulls mozilla.org site related data and aggregates into one table
-# Final dataset used to power the Desktop Growth Datastudio dashboards
-# Sources are primarily google analytics properties but starting Jan 29th integration will be required for retrieving data from telemetry due to data collection migration
+# Final dataset used to power experiment sizing and reporting of performance by landing page
 
 import os
 from google.cloud import bigquery
@@ -10,7 +9,8 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s: %(levelname)s: %(message)s')
 
 # Set environment variable to authenticate using service account
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/gkaberere/Google Drive/Github/gkLocalAppsServiceAccount.json'
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/gkaberere/Google Drive/Github/gkLocalAppsServiceAccount.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'moz-mktg-prod-001-app-engine-GAMozillaProdAccess.json'
 
 def calc_last_load_date(dataset_id, table_name):
     '''
@@ -88,126 +88,126 @@ def load_new_data(dataset_id, table_name, next_load_date, end_load_date):
           '{load_table_suffix}' as startDate,
           '{load_table_suffix}' as endDate
         ),
-        
-        sessionsTable as (
+                
+        sessionsTable as(SELECT
+          date,
+          language,
+          landingPage,
+          pagePathLevel1,
+          pageName,
+          visitIdentifier,
+          country,
+          source,
+          medium,
+          campaign,
+          adContent,
+          browser,
+          SUM(entrances) as sessions,
+          SUM(CASE WHEN browser != 'Firefox' THEN entrances ELSE 0 END) as nonFXSessions
+        FROM (
           SELECT
-            PARSE_DATE('%Y%m%d', date) as date,
-            CASE WHEN country IS NULL THEN '' ELSE country END as country,
-            CASE WHEN source IS NULL THEN '' ELSE source END as source,
-            CASE WHEN medium IS NULL THEN '' ELSE medium END as medium,
-            CASE WHEN campaign IS NULL THEN '' ELSE campaign END as campaign,
-            CASE WHEN content IS NULL THEN '' ELSE content END as content,
-            SUM(sessions) as sessions,
-            SUM(CASE WHEN browser != 'Firefox' THEN sessions ELSE 0 END) as nonFXSessions
-            FROM(
-              SELECT
-                date,
-                country,
-                source,
-                medium,
-                campaign,
-                content,
-                browser,
-                COUNT(DISTINCT visitIdentifier) as sessions
-              FROM(
-                  SELECT
-                    date AS date,
-                    CONCAT(CAST(fullVisitorId AS string),CAST(visitId AS string)) as visitIdentifier,
-                    geoNetwork.country as country,
-                    trafficSource.source as source,
-                    trafficSource.medium as medium,
-                    trafficSource.campaign as campaign,
-                    trafficSource.adcontent as content,
-                    device.browser as browser
-                  FROM
-                    `ga-mozilla-org-prod-001.65789850.ga_sessions_*`,
-                    UNNEST (hits) AS hits
-                  WHERE
-                    _TABLE_SUFFIX >= (SELECT parameters.startDate from parameters)
-                    AND _TABLE_SUFFIX <= (SELECT parameters.startDate from parameters)
-                    AND totals.visits = 1
-                  GROUP BY
-                    date,visitIdentifier, country, source, medium, campaign, content, browser)
-              GROUP BY
-                date, country, source, medium, campaign, content,browser)
-          GROUP BY date, country, source, medium, campaign, content),
-        
+            date,
+            device.language,
+            hits.page.pagePath as landingPage,
+            hits.page.pagePathLevel1 as pagePathLevel1,
+            CONCAT('/',REPLACE(hits.page.pagePathLevel2,'/',''),'/',REPLACE(hits.page.pagePathLevel3,'/',''),'/') as pageName,
+            CONCAT(CAST(fullVisitorId AS string),CAST(visitId AS string)) as visitIdentifier,
+            geoNetwork.country,
+            trafficSource.source,
+            trafficSource.medium,
+            trafficSource.campaign,
+            trafficSource.adContent,
+            device.browser,
+            CASE WHEN hits.isEntrance IS NOT NULL THEN 1 ELSE 0 END as entrances
+          FROM `ga-mozilla-org-prod-001.65789850.ga_sessions_*`, UNNEST (hits) as hits
+          WHERE
+            _TABLE_SUFFIX >= (SELECT startDate FROM parameters)
+            AND _TABLE_SUFFIX <= (SELECT endDate FROM parameters))
+        GROUP BY date, language, landingPage, pagePathLevel1, pageName, visitIdentifier, country, source, medium, campaign, adContent, browser),
         
         downloadsTable as(
         SELECT
-          PARSE_DATE('%Y%m%d', date) as date,
-          CASE WHEN country IS NULL THEN '' ELSE country END as country,
-          CASE WHEN source IS NULL THEN '' ELSE source END as source,
-          CASE WHEN medium IS NULL THEN '' ELSE medium END as medium,
-          CASE WHEN campaign IS NULL THEN '' ELSE campaign END as campaign,
-          CASE WHEN content IS NULL THEN '' ELSE content END as content,
+          date,
+          language,
+          visitIdentifier,
+          country,
+          source,
+          medium,
+          campaign,
+          adContent,
+          browser,
           SUM(IF(downloads > 0,1,0)) as downloads,
           SUM(IF(downloads > 0 AND browser != 'Firefox',1,0)) as nonFXDownloads
         FROM (SELECT
           date AS date,
-          fullVisitorId as visitorId,
-          visitNumber as visitNumber,
-          geoNetwork.country as country,
-          trafficSource.source as source,
-          trafficSource.medium as medium,
-          trafficSource.campaign as campaign,
-          trafficSource.adcontent as content,
+          CONCAT(CAST(fullVisitorId AS string),CAST(visitId AS string)) as visitIdentifier,
+          device.language,
+          geoNetwork.country,
+          trafficSource.source,
+          trafficSource.medium,
+          trafficSource.campaign,
+          trafficSource.adContent,
           device.browser as browser,
           SUM(IF (hits.eventInfo.eventAction = "Firefox Download",1,0)) as downloads
         FROM
           `ga-mozilla-org-prod-001.65789850.ga_sessions_*`,
           UNNEST (hits) AS hits
         WHERE
-          _TABLE_SUFFIX >= (SELECT parameters.startDate from parameters)
-          AND _TABLE_SUFFIX <= (SELECT parameters.startDate from parameters)
+          _TABLE_SUFFIX >= (SELECT startDate FROM parameters)
+          AND _TABLE_SUFFIX <= (SELECT endDate FROM parameters)
           AND hits.type = 'EVENT'
           AND hits.eventInfo.eventCategory IS NOT NULL
           AND hits.eventInfo.eventLabel LIKE "Firefox for Desktop%"
         GROUP BY
-          date,visitorID, visitNumber, country, source, medium, campaign, content, browser)
-        GROUP BY date, country, source, medium, campaign, content
-        ),
+          date,visitIdentifier, language, country, source, medium, campaign, adContent, browser)
+        GROUP BY date, language, visitIdentifier, country, source, medium, campaign, adContent, browser),
         
-        siteData as (
+        joinedTable as (
         SELECT
           sessionsTable.date,
+          sessionsTable.visitIdentifier,
+          sessionsTable.language,
+          sessionsTable.landingPage,
+          sessionsTable.pagePathLevel1,
+          sessionsTable.pageName,
           sessionsTable.country,
           sessionsTable.source,
           sessionsTable.medium,
           sessionsTable.campaign,
-          sessionsTable.content,
-          SUM(sessionsTable.sessions) as sessions,
-          SUM(sessionsTable.nonFXSessions) as nonFXSessions,
-          SUM(downloadsTable.downloads) as downloads,
-          SUM(downloadsTable.nonFXDownloads) as nonFXDownloads
+          sessionsTable.adContent,
+          sessionsTable.browser,
+          sessionsTable.sessions,
+          sessionsTable.nonFXSessions,
+          downloadsTable.downloads,
+          downloadsTable.nonFxDownloads
         FROM
           sessionsTable
-        LEFT JOIN
+        FULL JOIN
           downloadsTable
         ON
           sessionsTable.date = downloadsTable.date
-          AND sessionsTable.country = downloadsTable.country
-          AND sessionsTable.source = downloadsTable.source
-          AND sessionsTable.medium = downloadsTable.medium
-          AND sessionsTable.campaign = downloadsTable.campaign
-          AND sessionsTable.content = downloadsTable.content
-        GROUP BY
-          date, country, source, medium, campaign, content
-        ),
+          AND sessionsTable.visitIdentifier = downloadsTable.visitIdentifier
+        WHERE sessionsTable.sessions != 0)
         
-        cleanedSiteData as (
         SELECT
-          siteData.*,
-          standardizedCountryList.standardizedCountry as countryCleaned
-        FROM
-          siteData
-        LEFT JOIN
-          `lookupTables.standardizedCountryList` as standardizedCountryList
-        ON
-          siteData.country = standardizedCountryList.rawCountry
-        )
-        
-        SELECT * from cleanedSiteData
+        PARSE_DATE('%Y%m%d', date) as date,
+        language,
+        landingPage,
+        pagePathLevel1,
+        pageName,
+        country,
+        source,
+        medium,
+        campaign,
+        adContent,
+        browser,
+        SUM(sessions) as sessions,
+        SUM(nonFXSessions) as nonFXSessions,
+        SUM(downloads) as downloads,
+        SUM(nonFXDownloads) as nonFXDownloads
+        FROM joinedTable
+        GROUP BY
+          date, language, landingPage, pagePathLevel1, pageName, country, source, medium, campaign, adContent, browser
         """
 
         # Run Load Job
@@ -229,7 +229,7 @@ def load_new_data(dataset_id, table_name, next_load_date, end_load_date):
 def run_site_metrics_update():
     # Find the last date when data was loaded into the table
     read_dataset_id = 'desktop'
-    read_table_name = 'website_metrics'
+    read_table_name = 'website_landing_page_metrics'
     last_load_date = calc_last_load_date(read_dataset_id, read_table_name)
 
     # Set dates required for loading new data
